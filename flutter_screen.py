caa29b2cbf17c81855633109ff0fe8c850fa6164
Faizal Khalifa <faizalfkhalifa786@gmail.com>
2023-07-17 22:11:55 +0530

    fix bug and add router name in flutter_screen.py

--- flutter_screen.py
+++ flutter_screen.py
@@ -4,13 +4,15 @@
 import os
 
 
-def create_screen_file(folder_name):
+def create_screen_file(folder_name: str, router_name: str) -> str:
     return f"""
 import 'package:flutter/material.dart';
 
 class {folder_name.capitalize()}Screen extends StatelessWidget {{
   const {folder_name.capitalize()}Screen({{super.key}});
 
+  static const String routerName = '/{router_name}';
+  
   @override
   Widget build(BuildContext context) {{
     return const Scaffold(
@@ -27,7 +29,9 @@ class {folder_name.capitalize()}Screen extends StatelessWidget {{
 
 folder_name = input("Enter a Screen Name: ")
 
-if folder_name == "":
+router_name = input("Enter a Router Name: ")
+
+if folder_name == "" or router_name == "":
     exit(1)
 
 screen_structure = {
@@ -47,7 +51,7 @@ screen_structure = {
     "views": [
         {
             "file_name": f"{folder_name}_screen.dart",
-            "content": f"{create_screen_file(folder_name)}"
+            "content": f"{create_screen_file(folder_name, router_name)}"
         },
     ],
 }
@@ -61,11 +65,19 @@ def main():
 
         for sub_folder in screen_structure:
             sub_folder_current_path = os.path.join(path, sub_folder)
+
             os.mkdir(sub_folder_current_path)
 
             for item in screen_structure[sub_folder]:
+
+                sub__folder_current_path = sub_folder_current_path
+
+                if item.get('folder') is not None :
+                    sub__folder_current_path = os.path.join(sub__folder_current_path, item.get('folder'))
+                    os.mkdir(sub__folder_current_path)
+
                 if item.get('file_name') is not None and item.get('file_name'):
-                    with open(os.path.join(sub_folder_current_path, item.get('file_name')), 'w') as file:
+                    with open(os.path.join(sub__folder_current_path, item.get('file_name')), 'w') as file:
                         file.write(item.get('content'))
 
     except OSError:
